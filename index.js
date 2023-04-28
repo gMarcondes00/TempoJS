920a7c9ba213061ad380843b5915a509d3545c0f
gMarcondes00 <g.os.marcondes@gmail.com>
2023-04-28 14:10:49 -0300

    Basic Visual

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 function App(){
-    return 
+    const [city, setCity] = React.useState('');
+    const apiId = '&appid=c8ad00f8bb6ef6d755eec02447995793';
+
+    const getWeather = (cityName) => {
+        axios.get(`'https://api.openweathermap.org/data/2.5/weather?'q=${cityName}${apiId}`)
+            .then(res=>{res.json()})
+            .then(res=>console.log(res))
+    }
+
+    return (
+        <div className='header'>
+            <h1>Insira o Nome da cidade!</h1>
+            <div className='card'>
+                <input onChange={e=>{setCity(e.target.value)}} type='text' className='cidade'/><br/>
+                <Search />
+                <button className='btn btn-primary' onClick={() => getWeather(city)}>Enviar</button>
+            </div>
+        </div>
+    )
 }
+
+
+const Search = () => {
+    
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))

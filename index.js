7110b1b3b459c8fb7fb70bb787386e00ca533ba3
gMarcondes00 <g.os.marcondes@gmail.com>
2023-05-02 10:23:51 -0300

    backgroundImages adicionadas

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+//Mudar a imagem do background
+const imgS = [
+    {main: 'Clouds', className: "nubladoIMG"},
+    {main: 'Mist', className: "nubladoIMG"},
+    {main: 'Thunderstorm', className: "tempestadeIMG"},
+    {main: 'Rain', className: "chuvaIMG"},
+    {main: 'Clear', className: "clearIMG"}
+]
+
+const changeClass = (tempo) => {
+    const img = imgS.find((img)=> img.main === tempo);
+    return (`header ${img.className}`)
+}
+
 function App(){
     const [weather, setWeather] = React.useState(null);
     const [input, setInput] = React.useState('');
     const [city, setCity] = React.useState('');
     const [display, setDisplay] = React.useState('');
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c8ad00f8bb6ef6d755eec02447995793`;
-
+    
+    //Usar a API para ver o tempo
     React.useEffect(()=>{
         axios.get(url)
             .then(res=>{
@@ -15,6 +30,7 @@ function App(){
             })
     },[url])
 
+    //Atualizar a API
     const getWeather = () => {
         setCity(input.toLowerCase().replace(/\s/g, "%20"));
         setDisplay(input.toUpperCase());
@@ -23,6 +39,7 @@ function App(){
                 setWeather(res.data)
             })
     }
+
     if(!weather){
     return (
         <div className='header'>
@@ -34,7 +51,7 @@ function App(){
         </div>
     )} else {
         return(
-        <div className='header'>
+        <div className={changeClass(weather.weather[0].main)}>
             <div className='card input-box'>
                 <h1>Insira o nome da cidade!</h1>
                 <input onChange={(e)=>setInput(e.target.value)} type='text' className='cidade'/><br/>
@@ -42,6 +59,7 @@ function App(){
                 <div className='card weather'>
                     <h1>{display}</h1><p>{weather.sys.country}</p><hr/>
                     <h1>{weather.weather.main}</h1>
+                    <h1>{weather.weather[0].main}</h1>
                     <h1><i className="fa-solid fa-temperature-three-quarters"></i> {Math.round(weather.main.temp -273)}ºC</h1>
                     <h1><i className="fa-solid fa-droplet"></i> {weather.main.humidity}%</h1>
                     <h1><i className="fa-solid fa-wind"></i> {Math.round(weather.wind.speed * 3.6)} km/h</h1>
